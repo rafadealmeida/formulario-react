1176d38265b6e7bcbfc95b2786da898a95e66fe4
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-07-29 16:59:42 -0300

    Usando Hook
    1- Importe o hook de useState
    2- Passo as variável para cada estado, em um array onde o primeiro valor é referência para o valor do estado atual, e o segundo é a função que alterará este estado
    3- Coloco esses controles de estados nos componentes, não esquecendo de passar o value com o valor do estado atual

--- formulario-react/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ formulario-react/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,23 +1,81 @@
-import React from "react";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Switch from '@mui/material/Switch';
-
-
-
-const label = "Promoção"
+import React, { useState } from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [promocao, setPromocao] = useState(true);
+  const [novidade, setNovidade] = useState(true);
+
   return (
-    <form className="formulario">
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+      className="formulario"
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={cpf}
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
 
-    <TextField id="nome" label="Nome" variant="outlined" fullWidth />
-    <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth/>
-    <TextField id="cpf" label="CPF" variant="outlined" fullWidth/>
-     
-    <Switch {...label} defaultChecked/> 
+      <FormControlLabel
+        control={
+          <Switch
+            onChange={(event) => {
+              setPromocao(event.target.checked);
+            }}
+            defaultChecked={promocao}
+            name="promocao"
+          />
+        }
+        label="Promoção"
+      />
+      
+      <FormControlLabel
+        control={
+          <Switch
+            onChange={(event) => {
+              setNovidade(event.target.checked);
+            }}
+            defaultChecked={novidade}
+            name="novidade"
+          />
+        }
+        label="Novidade"
+      />
+
       <Button variant="contained"> Cadastrar</Button>
-
     </form>
   );
 }
