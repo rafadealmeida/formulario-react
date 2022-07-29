06ce2fda6ea15b4b722d2b12a28c1a03b49e4340
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-07-29 13:51:54 -0300

    Utilizando lib material ui

--- formulario-react/src/App.js
+++ formulario-react/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
-function App() {
-  return (
-    <div className="App">
-     <h1 className="formulario__titulo">Formulário</h1>
-    </div>
-  );
+import { Container } from '@mui/material';
+
+class App extends Component {
+  render() {
+    return (
+      <Container maxWidth ="sm" component = "article">
+        <div className="App">
+          <h1 className="formulario__titulo">Formulário</h1>
+          <FormularioCadastro />
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default App;
