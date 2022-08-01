1c5fb6594be7e2b9240367701d4e0422b618bc33
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-08-01 20:30:21 -0300

    Renderização condicional
    1- Criando variável,  para guardar o estado (usando hook)
    2-Criamos um função dentro do function component ,para aproveitar suas props.
    3-Dentro desta função usamos o m´wtodo switch, do JS puro, para trocar render
    4- No retorno da function componets coloco essa função para renderizar, assim podemos modificar o que será renderizado

--- formulario-react/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ formulario-react/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState}from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuarios";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function formularioAtual(etapa){
+    switch (etapa){
+      case 0:
+        return <DadosUsuario/>
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
+      case 2:
+        return <DadosEntrega/>
+    }
+  }
+  
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario/>
-      <DadosEntrega/>
+    {formularioAtual(etapaAtual)}  
+      
     </>
   );
 }
